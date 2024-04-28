cb73c1cd021cea42f646abfeaba383bfa0f51c55
Charles042 <ekechukwuma20@gmail.com>
2024-04-28 11:12:46 +0100

    Added implemented the search input

--- src/Components/AutoComplete.jsx
+++ src/Components/AutoComplete.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+impor
 
 const AutoComplete = () => {
   return (
-    <div>AutoComplete</div>
+    <div className='w-50 p-5 rounded max auto'>
+      <div className='form-floating dropdown'>
+      <input style={{background: "rgba(145,158,171,0.04"}} 
+      type="text" id='search'  className='form-control' placeholder='Search' autoComplete='off'/>
+      <label htmlFor="search"> Search</label>
+      </div>
+
+    </div>
   )
 }
 
